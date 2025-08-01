cb13bbeaba75bb3740a65a3f163faa18408faee6
Ehhthan <ethanhaynes46@gmail.com>
2025-08-01 15:19:34 -0500

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/book-display/",
+
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    target: 'es2015',
+    rollupOptions: {
+      input: {
+        widget: path.resolve(__dirname, 'src/main.tsx'),
+        index: path.resolve(__dirname, 'index.html'),
+      },
+      output: {
+        entryFileNames: '[name].js',
+        assetFileNames: '[name][extname]',
+        chunkFileNames: '[name].js',
+      },
+      external: ['react'],
+    },
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
+
 })
