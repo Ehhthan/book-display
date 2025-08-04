f2f07dad2f26a2c6c8cffa9260f2ec716ceb1658
Ehhthan <ethanhaynes46@gmail.com>
2025-08-04 00:54:41 -0500

    new base64

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './common.css'
 import {BookDisplay} from './component.tsx'
+import './common.css'
+import { Base64 } from "js-base64";
 
 function init() {
 	const rootEl = document.getElementById('minecraft-book');
 	if (!rootEl) return;
 
-	const author = rootEl.dataset.author || "Unknown";
-	const title = rootEl.dataset.title || "Untitled";
+	const author = rootEl.dataset.author || "Unknown Author";
+	const title = rootEl.dataset.title || "VW50aXRsZWQ="; // in base 64 because of character escapes
 	const pages = rootEl.dataset.pages || "WyJObyBUZXh0Il0="; // in base 64 because of character escapes
 
 	createRoot(rootEl).render(
 		<StrictMode>
-			<BookDisplay title={title} author={author} pages={JSON.parse(atob(pages))} />
+			<BookDisplay title={Base64.decode(title)} author={author} pages={JSON.parse(Base64.decode(pages))} />
 		</StrictMode>,
 	);
 }
