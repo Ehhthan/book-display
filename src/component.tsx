8d8d8178e05bdff2482642a2363dcc35ce526954
Ehhthan <ethanhaynes46@gmail.com>
2025-08-20 19:01:38 -0500

    tooltip

--- src/component.tsx
+++ src/component.tsx
@@ -8,8 +8,23 @@ interface BookMeta {
     pages: string[];
 }
 
+export function ItemTooltip({lines}: {lines: string[]}) {
+    const component = MiniMessage
+        .miniMessage()
+        .deserialize(lines.join("<br>"));
+
+    return (
+        <div className={'item-tooltip'}>
+            <div className={"item-tooltip-text"}
+                 dangerouslySetInnerHTML={{__html: MiniMessage.miniMessage().toHTML(component)}}
+            />
+        </div>
+    );
+}
+
 export function BookDisplay({author, title, pages}: BookMeta) {
     const [page, setPage] = useState(1);
+    const [isHovered, setHovered] = useState(false);
 
     const maxPages = pages.length;
 
@@ -17,39 +32,46 @@ export function BookDisplay({author, title, pages}: BookMeta) {
         .miniMessage()
         .deserialize(pages[page - 1]);
 
-    return (
-        <div className={'book-container'}>
-            <div className={"book-page-counter book-text"}>
-                Page {page} of {maxPages}
-            </div>
-
-            <div className={"book-text book-text-content"}
-                 dangerouslySetInnerHTML={{__html: MiniMessage.miniMessage().toHTML(component)}}
-            />
-
-            <div
-                className={"book-button book-backward"}
-                draggable={false}
-                hidden={page <= 1}
-                onClick={() => {
-                    if (page - 1 > 0) {
-                        setPage(page - 1);
-                    }
-                }}
-            />
-
-            <div
-                className={"book-button book-forward"}
-                draggable={false}
-                hidden={page >= maxPages}
-                onClick={() => {
-                    if (page + 1 <= maxPages) {
-                        setPage(page + 1);
-                    }
-                }}
-            />
-            <div className={"book-info"} draggable={false} title={"Title: " + title + "\n" + "Author: " + author }/>
-
+    return <div className={'book-container'}>
+        <div className={"book-page-counter book-text"}>
+            Page {page} of {maxPages}
         </div>
-    );
+
+        <div className={"book-text book-text-content"}
+             dangerouslySetInnerHTML={{__html: MiniMessage.miniMessage().toHTML(component)}}
+        />
+
+        <div
+            className={"book-button book-backward"}
+            draggable={false}
+            hidden={page <= 1}
+            onClick={() => {
+                if (page - 1 > 0) {
+                    setPage(page - 1);
+                }
+            }}
+        />
+
+        <div
+            className={"book-button book-forward"}
+            draggable={false}
+            hidden={page >= maxPages}
+            onClick={() => {
+                if (page + 1 <= maxPages) {
+                    setPage(page + 1);
+                }
+            }}
+        />
+        <div className={"book-info"}
+             draggable={false}
+             onMouseOver={() => setHovered(true)}
+             onMouseOut={() => setHovered(false)}
+        >
+            {isHovered &&
+                <div className={"item-tooltip-container"}>
+                    <ItemTooltip lines={[`<white>Title: ${title}`, `Author: ${author}`]} />
+                </div>
+            }
+        </div>
+    </div>;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -9,12 +9,13 @@ function init() {
 	if (!rootEl) return;
 
 	const author = rootEl.dataset.author || "Unknown Author";
-	const title = rootEl.dataset.title || "VW50aXRsZWQ="; // in base 64 because of character escapes
-	const pages = rootEl.dataset.pages || "WyJObyBUZXh0Il0="; // in base 64 because of character escapes
+	const title = Base64.decode(rootEl.dataset.title || "VW50aXRsZWQ="); // in base 64 because of character escapes
+	const pages = Base64.decode(rootEl.dataset.pages || "WyJObyBUZXh0Il0="); // in base 64 because of character escapes
+
 
 	createRoot(rootEl).render(
 		<StrictMode>
-			<BookDisplay title={Base64.decode(title)} author={author} pages={JSON.parse(Base64.decode(pages))} />
+			<BookDisplay title={title} author={author} pages={JSON.parse(pages)} />
 		</StrictMode>,
 	);
 }
