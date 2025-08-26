b0d1fd829b35bf4f43f64ad33bdb84cf2f50e2db
Ehhthan <ethanhaynes46@gmail.com>
2025-08-26 09:54:27 -0500

    tooltip

--- src/component.tsx
+++ src/component.tsx
@@ -32,46 +32,50 @@ export function BookDisplay({author, title, pages}: BookMeta) {
         .miniMessage()
         .deserialize(pages[page - 1]);
 
-    return <div className={'book-container'}>
-        <div className={"book-page-counter book-text"}>
-            Page {page} of {maxPages}
-        </div>
-
-        <div className={"book-text book-text-content"}
-             dangerouslySetInnerHTML={{__html: MiniMessage.miniMessage().toHTML(component)}}
-        />
-
-        <div
-            className={"book-button book-backward"}
-            draggable={false}
-            hidden={page <= 1}
-            onClick={() => {
-                if (page - 1 > 0) {
-                    setPage(page - 1);
-                }
-            }}
-        />
-
-        <div
-            className={"book-button book-forward"}
-            draggable={false}
-            hidden={page >= maxPages}
-            onClick={() => {
-                if (page + 1 <= maxPages) {
-                    setPage(page + 1);
-                }
-            }}
-        />
-        <div className={"book-info"}
-             draggable={false}
-             onMouseOver={() => setHovered(true)}
-             onMouseOut={() => setHovered(false)}
-        >
-            {isHovered &&
-                <div className={"item-tooltip-container"}>
-                    <ItemTooltip lines={[`<white>Title: ${title}`, `Author: ${author}`]} />
+    return (
+        <>
+            <div className={'book-page'}>
+                <div className={"book-page-counter book-text"}>
+                    Page {page} of {maxPages}
                 </div>
-            }
-        </div>
-    </div>;
+
+                <div className={"book-text book-text-content"}
+                     dangerouslySetInnerHTML={{__html: MiniMessage.miniMessage().toHTML(component)}}
+                />
+
+                <div
+                    className={"book-button book-backward"}
+                    draggable={false}
+                    hidden={page <= 1}
+                    onClick={() => {
+                        if (page - 1 > 0) {
+                            setPage(page - 1);
+                        }
+                    }}
+                />
+
+                <div
+                    className={"book-button book-forward"}
+                    draggable={false}
+                    hidden={page >= maxPages}
+                    onClick={() => {
+                        if (page + 1 <= maxPages) {
+                            setPage(page + 1);
+                        }
+                    }}
+                />
+                <div className={"book-info"}
+                     draggable={false}
+                     onMouseOver={() => setHovered(true)}
+                     onMouseOut={() => setHovered(false)}
+                />
+                {isHovered &&
+                    <div className={"item-tooltip-container"}>
+                        <ItemTooltip lines={[`<white>Title: ${title}`, `Author: ${author}`, "wdwdwddwwdwddwwdwd"]} />
+                    </div>
+                }
+            </div>
+
+        </>
+    );
 }
